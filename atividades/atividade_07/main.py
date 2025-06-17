6665dbaef27d02f37509bf858f7adde0b03ad9ff
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-06-17 17:19:17 -0300

    Aula do dia 17/06/2025.

---
+++ atividades/atividade_07/main.py
@@ -0,0 +1,17 @@
+"""
+# TODO - atividade: Crie um programa que faça as seguintes operações:
+- Inserir dados de novo usuário
+- Exibir lista de usuários
+- Alterar dados de um usuário já cadastrado
+- Excluir usuário da lista
+- Sair do programa
+Os dados a serem cadastrados serão os seguintes:
+- Nome
+- Data de Nascimento
+- E-mail
+- CPF
+- Telefone
+- Profissão
+- Gênero
+# NOTE - a lista deve começar vazia
+"""
